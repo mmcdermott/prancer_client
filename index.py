27f5e9b7e8612b25be83f55dcf2aa9f19e17c022
Matthew McDermott <mattmcdermott8@gmail.com>
2021-07-28 17:05:11 -0400

    Removed server; stripped dependencies.

--- index.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from config import BaseConfig
-from flask_bcrypt import Bcrypt
-
-app = Flask(__name__, static_folder="./static/dist", template_folder="./static")
-app.config.from_object(BaseConfig)
-app.secret_key = "aoij4w/ijsaI$2JASIJ1ijlasij"
-bcrypt = Bcrypt(app)
--- tests/test_api.py
+++
@@ -1,8 +0,0 @@
-from testing_config import BaseTestConfig
-import json
-
-
-class TestAPI(BaseTestConfig):
-    def test_get_spa_from_index(self):
-        result = self.app.get("/")
-        self.assertIn('<html>', result.data.decode("utf-8"))
